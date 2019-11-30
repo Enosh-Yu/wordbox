ca1db013b8bf3b4ecabd609f34b9a5f2c232459e
Enosh Yu <yjh6079@gmail.com>
2019-11-30 11:42:43 +0900

    check

--- src/component/WordBox.js
+++ src/component/WordBox.js
@@ -2,13 +2,17 @@ import React, {useEffect, useRef, useState} from 'react';
 import SmallBox from './SmallBox';
 import _ from 'lodash';
 
+const answers = [
+    [[0, 0], [0, 1], [0, 2], [0, 3]]
+];
+
 const WordBox = () => {
     const charactors = [
         [
-            {alphabet: 'a', isChecked: false},
-            {alphabet: 'a', isChecked: false},
-            {alphabet: 'a', isChecked: false},
-            {alphabet: 'a', isChecked: false},
+            {alphabet: 'l', isChecked: false},
+            {alphabet: 'i', isChecked: false},
+            {alphabet: 'n', isChecked: false},
+            {alphabet: 'e', isChecked: false},
             {alphabet: 'a', isChecked: false},
         ],
         [
@@ -61,16 +65,36 @@ const WordBox = () => {
     //console.log("wordbox render");
 
     useEffect(()=>{
-        console.log(activeArr);
+        //console.log(activeArr);
         if (activeArr.length === 5) {
             setActiveArr([]);
         }
+        checkAnswer();
     }, [activeArr]);
+
     const isActive = (x, y) => {
         const find = _.findIndex(activeArr, (el)=>{
             return JSON.stringify(el) === JSON.stringify([x, y]);
         });
         return find !== -1;
+    }
+
+    const checkAnswer = () => {
+        answers.forEach(item=>{
+            console.log("checkAnswer");
+            console.log(item);
+            const findWord = item.every((el)=>{
+                for (let i = 0 ; i < activeArr.length ; i++) {
+                    if (JSON.stringify(el) === JSON.stringify(activeArr[i])) {
+                        return true;
+                    }
+                }
+                return false;
+            });
+            if (findWord) {
+                console.log("find");
+            }
+        });
     }
     
     return (
@@ -81,8 +105,10 @@ const WordBox = () => {
                         <>
                         {
                             arr.map((item, key2)=>(
-                                <SmallBox item={item} x={key2} y={key} onClicked={handleCheck} 
-                                clickedLength={activeArr.length} active={isActive(key2, key)}/>
+                                <SmallBox 
+                                    item={item} x={key2} y={key} onClicked={handleCheck} 
+                                    clickedLength={activeArr.length} active={isActive(key2, key)} 
+                                    isAnswerChecked={false}/>
                             ))
                         }
                         </>
